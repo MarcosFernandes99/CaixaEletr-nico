404315e58ed3682639fce82d02b80b2e6e5b9143
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-24 21:02:43 -0300

    Criando algoritmo Caixa Eletronico

--- Ex.CaixaEletronico.js
+++ Ex.CaixaEletronico.js
@@ -1,2 +1,14 @@
 // DESAFIO CAIXA ELETRONICO
 
+var nome, cpf, opcao
+var saldoTotal = 1000
+var valor = 0
+
+
+nome = prompt(`Insira o seu nome`)
+cpf = parseFloat(prompt(`Insira o seu cpf`))
+opcao = prompt(`1 - Saque/ 2 - Depósito`)
+valor = parseInt(prompt(`Qual o valor`))
+
+
+
