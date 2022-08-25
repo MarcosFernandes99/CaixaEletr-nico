377591cb845105f1f615209410a5363a652324c3
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-24 21:37:51 -0300

    Adcionando comando de média e maior valor

--- Ex.CaixaEletronico.js
+++ Ex.CaixaEletronico.js
@@ -5,13 +5,23 @@ var saldoTotal = 1000
 var valor = 0
 var continuarCadastro = 0
 var continuar = true
+var maiorValorInserido = 0
+var mediaValores = 0
+var contadorMedia = 0
+var arrayValores = []
+var contadorArray = 0
 
 nome = prompt(`Insira o seu nome`)
 cpf = parseFloat(prompt(`Insira o seu cpf`))
+
 while(continuar){
 opcao = prompt(`1 - Saque/ 2 - Depósito`)
-console.log(`Saldo Total Atual: ${saldoTotal}`)
+console.log(`Saldo Total: ${saldoTotal}`)
 valor = parseInt(prompt(`Qual o valor`))
+contadorMedia++
+mediaValores = mediaValores + valor
+arrayValores[contadorArray] = valor
+contadorArray++
 
 if(opcao == 1){
     saldoTotal = saldoTotal - valor
@@ -19,11 +29,17 @@ if(opcao == 1){
 else{
     saldoTotal = saldoTotal + valor
 }
-console.log(`Saldo Total: ${saldoTotal}`)
+console.log(`Saldo Total Atual: ${saldoTotal}`)
     continuarCadastro = prompt(`1 - Continuar/2 - Encerrar`)
     if(continuarCadastro != 1){
         continuar = false
+    }  
+    if(valor > maiorValorInserido){
+        maiorValorInserido = valor
     }
-    
-    
 }
+
+console.log(`Maior valor inserido: ${maiorValorInserido}`)
+mediaValores = mediaValores/contadorMedia
+console.log(`Média dos valores inseridos: ${mediaValores}`)
+console.log(`Todos os valores inseridos: ${arrayValores}, CPF: ${cpf}`)
